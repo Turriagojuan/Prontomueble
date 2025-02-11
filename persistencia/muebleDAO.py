a0d271b92deb9b67aa59ba77d2ef789cabad82a4
Daniel <dagn61@gmail.com>
2025-02-10 22:31:25 -0500

    Cambios html

--- persistencia/muebleDAO.py
+++ persistencia/muebleDAO.py
@@ -6,29 +6,9 @@ class MuebleDAO:
         conexion = Conexion.obtener_conexion()
         cursor = conexion.cursor()
         cursor.execute("""
-        SELECT 
-            m.id_mueble, 
-            m.nombre, 
-            m.alto, 
-            m.largo, 
-            m.ancho, 
-            ma.nombre_mat AS material, 
-            c.nombre_color AS color, 
-            t.nombre AS tipo,
-            s.preciou
-        FROM 
-            mueble m
-        JOIN 
-            material ma ON m.id_material = ma.id_material
-        JOIN 
-            color c ON m.id_color = c.id_color
-        JOIN 
-            tipo t ON m.id_tipo = t.id_tipo
-        JOIN 
-            suministra s ON m.id_mueble = s.id_mueble
-        GROUP BY 
-            m.id_mueble, ma.nombre_mat, c.nombre_color, t.nombre, s.preciou
-    """)
+        SELECT *
+        FROM vista_catalogo_muebles
+                        """)
         muebles = cursor.fetchall()
         cursor.close()
         Conexion.liberar_conexion(conexion)
